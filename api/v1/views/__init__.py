d6fae20ad54b340ccdf67958ad8517c5c4f53f13
kepo402 <alawodeolayinka2@gmail.com>
2024-08-06 20:36:46 -0700

    new commit

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,10 +3,11 @@ from flask import Blueprint
 # Create a Blueprint named 'app_views' with the URL prefix '/api/v1'
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-# Import the routes from index.py, states.py, cities.py, amenities.py, and users.py
+# Import the routes from index.py, states.py, cities.py, amenities.py, users.py, and places.py
 from api.v1.views import index
 from api.v1.views import states
 from api.v1.views import cities
 from api.v1.views import amenities
 from api.v1.views import users
+from api.v1.views import places
 
