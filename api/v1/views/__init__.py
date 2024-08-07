63cdc68901c352d056fde69f09eeb981afd1e77d
kepo402 <alawodeolayinka2@gmail.com>
2024-08-06 19:51:04 -0700

    new commit

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,8 @@
+from flask import Blueprint
+
+# Create a Blueprint named 'app_views' with the URL prefix '/api/v1'
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# Import the routes from index.py to register them with the Blueprint
+from api.v1.views import index
+
